921532c8ca0a8a39e4f6264f48d15e515a15eb66
Drelofs <ali.shahid.1994@gmail.com>
2022-02-20 13:43:59 +0100

    Built theme in wordpress

--- wp-content/themes/bravo/lib/bravo.php
+++ wp-content/themes/bravo/lib/bravo.php
@@ -1,3 +1,24 @@
 <?php
 
-//Empty (for now...)
+// function get_members(){
+//     else{
+        
+//     }
+//     foreach($results as $result){
+//         $projects[] = [
+//             'id' => $result->ID,
+//             'title' => $result->post_title,
+//             'content' => $result->post_content,
+//             'permalink' => get_permalink($result->ID),
+//             'excerpt' => wp_trim_words($result->post_excerpt, 15, '...'),
+//             'image' => wp_get_attachment_image_src( get_post_thumbnail_id($result->ID), 'hd')[0],
+//             'company_logo' => get_field('project_company_logo', $result->ID),
+//             'goal' => get_field('project_goal', $result->ID),
+//             'budget' => get_field('project_budget', $result->ID),
+//             'startdate' => format_date(get_field('project_startdate', $result->ID)),
+//             'enddate' => format_date(get_field('project_enddate', $result->ID)),
+//             'side_note' => get_field('project_side_note', $result->ID)
+//         ];
+//     }
+//     return $projects;
+// }
--- wp-content/themes/bravo/page-home.php
+++ wp-content/themes/bravo/page-home.php
@@ -6,6 +6,8 @@ $data     = include('global-data.php');
 if ( have_posts() ) :
     while ( have_posts() ) : the_post(); 
         $data['title'] = get_the_title();
+        $data['header'] = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_id()), 'hd')[0];
+        // $data['content'] = get_the_content();
         if( have_rows('title_main') ):
             while ( have_rows('title_main') ) : the_row();
                 $data['title_main'] = [
@@ -14,10 +16,36 @@ if ( have_posts() ) :
                 ];
             endwhile;
         endif;
+        $data['main_description'] = get_field('main_description');
+
+        $args = array(
+            'post_type' => 'members',
+            'order' => 'ASC'
+        );
+        $results = get_posts($args);
+        foreach($results as $result){
+            $data['members'][] = [
+                'name' => $result->post_title,
+                'image' => wp_get_attachment_image_src( get_post_thumbnail_id($result->ID), 'hd')[0]
+            ];
+        }
+
+        $args = array(
+            'post_type' => 'concepts',
+            'order' => 'ASC'
+        );
+        $results = get_posts($args);
+        foreach($results as $result){
+            $data['concepts'][] = [
+                'name' => $result->post_title,
+                'image' => wp_get_attachment_image_src( get_post_thumbnail_id($result->ID), 'hd')[0],
+                'content' => $result->post_content
+            ];
+        }
     endwhile;
 endif;
 
-// print_r($data['title_main']);
+// print_r($data['members']);
 
 set_wp_header_footer();
 echo $compiler->render('index', $data);
